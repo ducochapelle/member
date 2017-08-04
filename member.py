2630ac20d68fe83c55b1f37dbef6279deaa9d66f
ducochapelle <duco.chapelle@gmail.com>
2017-08-04 10:16:20 +0200

    changed from unum to pint

--- member.py
+++ member.py
@@ -1,20 +1,24 @@
 ''' 
 Todo:
  - Welds
- - Plastic section
+ - Section: Plastic
  - Pins
+ - Section: Torsion
 '''
 
-from math import pi, sqrt
-from unum.units import N,mm,m,s,kg,t,Pa
-from unum import Unum
-kN = Unum.unit('kN' ,1e3*N)
-MPa = Unum.unit('MPa',1e6*Pa)
-g = 10*m/s/s
+from math import pi # pint doesn't like sqrt
 
-def Report(s):
-    print(s)
-
+def Report(*s):
+    ''' gets a [str,val,val,...,val]. If units are used, some
+    formatting is done, otherwise not.'''
+    txt = s[0]
+    if type(s[1]) not in [type(1),type(1.)]:
+        vs = [x.to_base_units() for x in s[1:]]
+        print(txt.replace("{}","{:~.4g}").format(*vs))
+    else:
+        vs = s[1:]
+        print(txt.replace("{}","{:.4g}").format(*vs))
+              
 class Material():
     ''' Redefine all variables if to be used 
     with units.'''
@@ -110,11 +114,13 @@ class Section():
             rz = ry
             wy = Dy-dy
             wz = Dz-dz
+            tz = Dz/2-dz/2
+            ty = Dy/2-dy/2
         elif form=="square solid" or form=="round solid":
             Dy = Do
             Dz = Do
-            dy = 0
-            dz = 0
+            dy = Do-Do
+            dz = Do-Do
             ry = Do/2
             rz = Do/2
             wy = Dy-dy
@@ -123,10 +129,10 @@ class Section():
             Dy = Dy
             Dz = Dz
             if not ty and not tz:
-                ty = 0
-                tz = 0
-                dy = 0
-                dz = 0
+                ty = Dz-Dz
+                tz = Dz-Dz
+                dy = Dz-Dz
+                dz = Dz-Dz
             else:
                 dy = Dy-ty-ty
                 dz = Dz-tz-tz
@@ -139,17 +145,22 @@ class Section():
             rz = Dz/2
             wy = ty
             wz = 2*tz # conservative shortcut
+            dy = Dz-Dz
+            dz = Dz-Dz
         elif form=="roman II":
             if Di:
                 Do = Di + 2*ty
+                dy = Do/2 + Di/2
             elif Do:
                 Di = Do - 2*ty
+                dy = Do/2 + Di/2
             else:
                 raise "missing dimension"
             ry = Dy/2
             rz = Dz/2
             wy = 2*ty
             wz = 2*tz
+            dz = Dz-Dz
         self.Dz = Dz
         self.Dy = Dy
         self.dz = dz
@@ -168,6 +179,7 @@ class Section():
             Iz = 1/12*(  Dz*Dy**3- dz*dy**3 )
             Qz = Dz*Dy/2*Dy/4-dz*dy/2*dy/4
             Qy = Dy*Dz/2*Dz/4-dy*dz/2*dz/4
+            # Jx = 
             # Qz = 1/8 *(  Dz*Dy**2- dz*dy**2 )
             # Qy = 1/8 *(  Dy*Dz**2- dy*dz**2 )
         elif form=="round solid" or form=="round hollow":
@@ -176,6 +188,7 @@ class Section():
             Iz = pi/4*( (Dz/2)**4-(dz/2)**4 )
             Qz =  2/3*( (Dy/2)**3-(dy/2)**3 )
             Qy =  2/3*( (Dz/2)**3-(dz/2)**3 )
+            # Jx = pi/2*( (Dy/2)**4-(dy/2)**4 )
         elif form=="roman I":
             so = Section("rectangle",Dy=Dy, Dz=Dz)
             sm = Section("rectangle",Dy=Dy, Dz=Dz-tz-tz)
@@ -205,10 +218,17 @@ class Section():
         assert Iy*wy/Qy <= A, "{}Iy{},wy{},Qy{},A{}".format(form,Iy,wy,Qy,A)
     def report(self):
         if self.form=="rectangle":
-            Report("Rectangle HxhxBxb: {}x{}x{}x{}".format(self.Dz,self.dz,self.Dy,self.dy))
+            Report("Rectangle HxhxBxb: {} x {} x {} x {}",self.Dz,self.dz,self.Dy,self.dy)
         elif self.form=="round hollow":
-            Report("Tube Dxt: {}x{}".format(self.Dz,self.tz))
-
+            Report("Tube Dxt: {} x {}",self.Dz,self.tz)
+        elif self.form=="round solid":
+            Report("Rod D: {}",self.Dz)
+        elif self.form=="square solid":
+            Report("Bar D: {}",sef.Dz)
+        elif self.form=="roman II":
+            Report("II Beam: {} x {} x {} x {} x {}",self.Dy,self.ty,self.Dz,self.tz,self.dy)
+        else:
+            Report("No report")
 
 class Load():
     '''
@@ -236,22 +256,32 @@ class Load():
     you know..
     '''
     
-    def __init__(self,  Fx=0, Fy=0, Fz=0,
-                        dx=0, dy=0, dz=0,
-                        Mx=0, My=0, Mz=0):
-        self.Fx = Fx
-        self.Fy = Fy
-        self.Fz = Fz
-        self.Mx = Mx + Fz*dy - Fy*dz
-        self.My = My + Fx*dz - Fz*dx
-        self.Mz = Mz + Fy*dx - Fx*dy
-        self.dx = dx
+    def __init__(self,  Fx=None, Fy=None, Fz=None,
+                        dx=None, dy=None, dz=None,
+                        Mx=None, My=None, Mz=None):
+        force = Fx if Fx else Fy if Fy else Fz if Fz else 0
+        force = force - force
+        moment = Mx if Mx else My if My else Mz if Mz else 0
+        moment = moment - moment
+        distance = dx if dx else dy if dy else dz if dz else 0
+        distance = distance - distance
+        self.Fx = Fx if Fx else force
+        self.Fy = Fy if Fy else force
+        self.Fz = Fz if Fz else force
+        self.Mx = Mx if Mx else moment
+        self.My = My if My else moment
+        self.Mz = Mz if Mz else moment
+        self.dx = dx if dx else distance
+        self.dy = dy if dy else distance
+        self.dz = dz if dz else distance
     def report(self):
         txt = 'Load:'
+        vs = []
         for l,v in zip("Fx Fy Fz Mx My Mz".split(),[self.Fx,self.Fy,self.Fz,self.Mx,self.My,self.Mz]):
             if v != 0:
-                txt += " {} {},".format(l,v)
-        Report(txt[:-1])
+                txt += " "+l+" {},"
+                vs += [v]
+        Report([txt[:-1]]+vs)
 
 class Member():
     def __init__(self, mat, sec, load, a=0.34,Le = 2.0):
@@ -262,38 +292,44 @@ class Member():
         self.Le = Le
         
         # stability
-        if load.dx != 0:
+        if load.dx != 0 and load.Fx != 0:
             I = min(sec.Iy, sec.Iz)
             Ncr = pi**2*mat.E*I/(Le*load.dx)**2
-            lam = sqrt(sec.A*mat.fy/Ncr)
+            lam = (sec.A*mat.fy/Ncr)**.5
             phi = 0.5*(1+a*(lam-0.2)+lam**2)
-            X = min(1,1/(phi+sqrt(phi**2-lam**2))) 
+            X = min(1,1/(phi+(phi**2-lam**2)**.5)) 
             self.Rb = mat.fy * sec.A * X
             self.stability = max(0,-load.Fx/self.Rb)
         else:
             self.Rb = False
-            self.stability = False
+            self.stability = 0
 
         # strength
+        if load.dx * load.dy * load.dz != 0:
+            Mx = load.Mx + load.Fz*load.dy - load.Fy*load.dz
+            My = load.My + load.Fx*load.dz - load.Fz*load.dx
+            Mz = load.Mz + load.Fy*load.dx - load.Fx*load.dy
+        else:
+            Mx,My,Mz = 0,0,0
+
         sx = load.Fx/sec.A
-        sy = load.My*sec.rz/sec.Iy
-        sz = load.Mz*sec.ry/sec.Iz
+        sy = (My*sec.rz/sec.Iy) if My != 0 else 0
+        sz = (Mz*sec.ry/sec.Iz) if Mz != 0 else 0
         ty = load.Fy*sec.Qz/sec.Iz/sec.wz
         tz = load.Fz*sec.Qy/sec.Iy/sec.wy
         sx,sy,sz,ty,tz=map(abs,[sx,sy,sz,ty,tz])
-        svm1 = sqrt((sx)**2+3*(ty+tz)**2)
+        svm1 = ((sx)**2+3*(ty+tz)**2)**.5
         svm2 = sx+sy+sz
         self.strength = max(svm1,svm2) / mat.fy
         
         # flexibility 
         epsilonx = load.Fx/sec.A/mat.E
-        self.displacement = epsilonx*load.dx
-        
+        self.displacement = epsilonx
+
+        self.stability *= self.strength-self.strength
         self.sec.report()
-        self.load.report()
-        print( "str:",round(self.strength,2), 
-               "sta:",round(self.stability,2),
-               "dis:",round(self.displacement,2))
+        # self.load.report()
+        Report( "str: {} sta: {} dis: {}",self.strength,self.stability,self.displacement)
 
 def WeldSection(form,a,t,L):
     '''
@@ -326,14 +362,9 @@ def WeldSection(form,a,t,L):
     if form=="2xPP":
         return Section("rectangle",Dz=L,dz=0,Dy=a+t+a,dy=t)
     if form=="1xPP":
-        pass
+        return Section("rectangle",Dz=L,Dy=a)
     if form=="2xFW":
-        pass
- 
-def Weld(mat,sec,load):
-    pass
-
-        
+        pass        
     
 class Hole():
     def __init__(self, mat, sec, load):
@@ -343,3 +374,4 @@ def M355(sec,load, a=0.34,Le = 2.0):
     return Member(Material(355), sec, load, a, Le)
 def Rect(**args):
     return Section("rectangle",**args)
+
